a18bc5bfa315c4b4243ea66c5e39e1f35c7f7260
Alvaro Muñoz Sanchez <alvaro@pwntester.com>
2022-05-16 15:44:30 +0200

    update

---
+++ lua/lualine/themes/nautilus_octonauts.lua
@@ -0,0 +1,38 @@
+local theme = require("nautilus.theme").apply({
+  mode = "octonauts",
+})
+
+local colors = theme.colors
+
+return {
+  normal = {
+    a = { bg = colors.bg, fg = colors.white },
+    b = { bg = colors.bg, fg = colors.blue_dim },
+    c = { bg = colors.bg, fg = colors.yellow }
+  },
+  insert = {
+    a = { bg = colors.bg, fg = colors.white },
+    b = { bg = colors.bg, fg = colors.blue_dim },
+    c = { bg = colors.bg, fg = colors.yellow }
+  },
+  visual = {
+    a = { bg = colors.bg, fg = colors.white },
+    b = { bg = colors.bg, fg = colors.blue_dim },
+    c = { bg = colors.bg, fg = colors.yellow }
+  },
+  replace = {
+    a = { bg = colors.bg, fg = colors.white },
+    b = { bg = colors.bg, fg = colors.blue_dim },
+    c = { bg = colors.bg, fg = colors.yellow }
+  },
+  command = {
+    a = { bg = colors.bg, fg = colors.white },
+    b = { bg = colors.bg, fg = colors.blue_dim },
+    c = { bg = colors.bg, fg = colors.yellow }
+  },
+  inactive = {
+    a = { bg = colors.bg, fg = colors.white },
+    b = { bg = colors.bg, fg = colors.blue_dim },
+    c = { bg = colors.bg, fg = colors.yellow }
+  }
+}
